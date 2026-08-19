ee58a37e65db102e6617faeda7bc99cba170fb07
Kubilay Agi <kubilay.agi@gmail.com>
2026-08-18 22:14:43 -0700

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = collections.defaultdict(list)
+        for crs, prq in prerequisites:
+            adj[crs].append(prq)
+
+        path = set()
+
+        def dfs(crs):
+            if crs in path:
+                return False
+            if not adj[crs]:
+                return True
+            path.add(crs)
+            for prq in adj[crs]:
+                if not dfs(prq):
+                    return False
+            path.remove(crs)
+            adj[crs] = []
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+
+        return True
